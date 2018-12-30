76343cf9498a34f45e70c591b48c08ac18f2f3db
RiverXu <sdxujianghe@126.com>
2018-12-30 22:37:30 +0800

    Annotations for deploy.sh

--- Jenkins/deploy.sh
+++ Jenkins/deploy.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
 
+# Make sure that maven has downloaded needed jars for "help:evaluate"
+mvn help:evaluate -Dexpression=project.name
+
 NAME=`mvn help:evaluate -Dexpression=project.name | grep "^[^\[]"`
 VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
 
+# Environment variables needed.
 JARNAME=$NAME-$VERSION
 BUILDDIR=target/
 DEPLOYDIR=/tmp/project-jar/
 
-
+# Create DELPOYDIR if needed
 if [ ! -d $DIR/backup ];then
    mkdir -p $DEPLOYDIR
 fi
 
-ps -ef | grep $JARFILE | grep -v grep | awk '{print $2}' | xargs kill -9
+# Kill previous SpringBoot processes.
+ps -ef | grep $NAME | grep -v grep | awk '{print $2}' | xargs kill -9
 
+# Remove previous jar.
 rm -f $DEPLOYDIR/$JARNAME
 
+# Copy new jar to DEPLOYDIR.
 cp $BUILDDIR/$JARNAME $DEPLOYDIR
 
+# Execute the jar and wait for a little log.
 java -jar $DEPLOYDIR/$JARNAME > $DEPLOYDIR/out.log &
 
 if [ $? = 0 ];then
