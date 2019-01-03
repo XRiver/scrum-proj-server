226e3086d251798d0e426e4186e1d7fb9ded150c
jackltx <42914370+jackltx@users.noreply.github.com>
2019-01-03 16:51:25 +0800

    Add files via upload
    January 3rd

--- src/main/java/com/nju/scrum/controller/HelloController.java
+++ src/main/java/com/nju/scrum/controller/HelloController.java
@@ -1,23 +1,15 @@
 package com.nju.scrum.controller;
 
-import com.nju.scrum.bean.Attraction;
-import com.nju.scrum.dao.AttractionMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.List;
-
+//ResponseBody注解写在类的上方，表示这个类的所有方法返回的数据直接写给浏览器，（如果是对象则转为json数据）
+@ResponseBody
 @Controller
+//两者相叠加等价于restcontroller
 public class HelloController {
-    @Autowired
-    AttractionMapper attractionMapper;
-    @ResponseBody
     @RequestMapping("/hello")
     public String hello() {
-        List<Attraction> list = attractionMapper.selectAll();
         return "hello world a third time";
     }
-
 }
